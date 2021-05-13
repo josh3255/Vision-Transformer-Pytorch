b95765630502d3db9af7cade66af3a4cfb794978
josh <josh3255@kangwon.ac.kr>
2021-05-13 16:28:54 +0900

    implement Multi Head Attention

--- model.py
+++ model.py
@@ -31,11 +31,46 @@ class PatchEmbedding(nn.Module):
         # embedded_data : Batch, Patch num(N) + 1, Embedding Size
         return embedded_data
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, head_num : int = 8, emb_size : int = 768):
+        super(MultiHeadAttention, self).__init__()
+        self.head_num = head_num
+        self.emb_size = emb_size
+
+        self.q_layer = nn.Linear(emb_size, emb_size)
+        self.k_layer = nn.Linear(emb_size, emb_size)
+        self.v_layer = nn.Linear(emb_size, emb_size)
+
+        self.projection = nn.Linear(emb_size, emb_size)
+
+    def forward(self, input):
+        # input : Batch, Patch num(N) + 1, Embedding Size
+        b, n, e = input.shape
+
+        queries = self.q_layer(input)
+        keys = self.k_layer(input)
+        values = self.v_layer(input)
+
+        queries = torch.reshape(queries, [b, self.head_num, n, int(e / self.head_num)])
+        keys = torch.reshape(keys, [b, self.head_num, n, int(e / self.head_num)])
+        values = torch.reshape(values, [b, self.head_num, n, int(e / self.head_num)])
+
+        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)
+        scailing_factor = self.emb_size ** (1/2)
+        attention_score = torch.softmax(energy, dim=-1) / scailing_factor
+
+        out = torch.einsum('bhal, bhlv -> bhav ', attention_score, values)
+
+        b, h, n, d = out.shape
+        out = torch.reshape(out, [b, n, int(h * d)])
+        out = self.projection(out)
+        # out : Batch, Patch num(N) + 1, Embedding Size
+        return out
 
 
 if __name__ == '__main__':
     x = torch.randn(8, 3, 224, 224)
     Net = PatchEmbedding()
     embedded = Net(x)
-    # Net2 = MultiHeadAttention()
-    # Net2(embedded)
+    Net2 = MultiHeadAttention()
+    Net2(embedded)
